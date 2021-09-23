a6678173cec2946da2a6fb962ab3e6eaaaedc5c9
dsv <dinosepulvedav@gmail.com>
2021-09-23 15:39:25 -0300

    modified for updated data on covid19 pandemic

--- covid19/getcovid19data.py
+++ covid19/getcovid19data.py
@@ -9,7 +9,7 @@ linecount = 1
 result = ''
 
 # Download COVID-19 CSV data from https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data
-url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv'
+url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 covidcsv = requests.get(url, allow_redirects=True)
 open('covidcsv', 'wb').write(covidcsv.content)
 
@@ -21,34 +21,22 @@ for line in lines:
 	if linecount == 1: # skip 1st line, because it's the header's names
 		linecount = linecount + 1
 	else:
-		# dateRep,day,month,year,cases,deaths,countriesAndTerritories,geoId,countryterritoryCode,
-		# popData2019,continentExp,Cumulative_number_for_14_days_of_COVID-19_cases_per_100000
-		parts  = line.split(sep)
-		day    = parts[1]
-		month  = parts[2]
-		year   = parts[3]
-		cases  = parts[4]
-		deaths = parts[5]
-		cat    = parts[6].replace('_', ' ') # countriesAndTerritories. Some contain underscores, that we replace for spaces
-		geoid  = parts[7]
-		ctc    = parts[8] # countryterritoryCode
+        #iso_code,continent,location,date,total_cases,new_cases,new_cases_smoothed,total_deaths,new_deaths,new_deaths_smoothed,total_cases_per_million,new_cases_per_million,new_cases_smoothed_per_million,total_deaths_per_million,new_deaths_per_million,new_deaths_smoothed_per_million,reproduction_rate,icu_patients,icu_patients_per_million,hosp_patients,hosp_patients_per_million,weekly_icu_admissions,weekly_icu_admissions_per_million,weekly_hosp_admissions,weekly_hosp_admissions_per_million,new_tests,total_tests,total_tests_per_thousand,new_tests_per_thousand,new_tests_smoothed,new_tests_smoothed_per_thousand,positive_rate,tests_per_case,tests_units,total_vaccinations,people_vaccinated,people_fully_vaccinated,total_boosters,new_vaccinations,new_vaccinations_smoothed,total_vaccinations_per_hundred,people_vaccinated_per_hundred,people_fully_vaccinated_per_hundred,total_boosters_per_hundred,new_vaccinations_smoothed_per_million,stringency_index,population,population_density,median_age,aged_65_older,aged_70_older,gdp_per_capita,extreme_poverty,cardiovasc_death_rate,diabetes_prevalence,female_smokers,male_smokers,handwashing_facilities,hospital_beds_per_thousand,life_expectancy,human_development_index,excess_mortality_cumulative,excess_mortality
+        parts  = line.split(sep)
+        isoco  = parts[0]
+        conti  = parts[1]
+        locat  = parts[2]
+        date   = parts[3].replace('-', '')
+        ncases  = parts[5].split('.')
+        deaths = parts[8].split('.')
+        cases  = str(ncases[0])
+        deaths  = str(deaths[0])
 
-		if len(ctc) > 0:
-			# Data contain Cases_on_an_international_conveyance_Japan,
-			# but we are not interested on it. Hence, filter it out.
+        if len(isoco) > 0:
+            # Data contain Cases_on_an_international_conveyance_Japan,
+            # but we are not interested on it. Hence, filter it out.
 
-			if len(ctc) < 3:
-				if cat.startswith('"'):
-					# "Bonaire, Saint Eustatius and Saba" contains commas in the country name.
-					# We need to reformat the name, as it's split into 2 different parts
-					fullname = parts[6].replace('"', '') + ',' + parts[7].replace('"', '')
-					result = year + month.zfill(2) + day.zfill(2) + cases.zfill(8) + deaths.zfill(8) + parts[9] + fullname.ljust(40)
-			else:
-				if ctc == 'CNG1925':
-					# Convert Taiwan country code to TWN
-					# Because it's easier for us later on MVS
-					ctc = 'TWN'
-
-				result = year + month.zfill(2) + day.zfill(2) + cases.zfill(8) + deaths.zfill(8) + ctc + cat.ljust(40)
-
-			print(result)
+            # There are also aggregated data by continents, so are filtered.
+            if len(isoco) < 4:
+                result = date + cases.zfill(8) + deaths.zfill(8) + isoco + locat.ljust(40)
+                print(result)
